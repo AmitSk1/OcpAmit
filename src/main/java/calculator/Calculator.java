8255eec805f22799d74028fa5a901fabdfddf972
Your Name <you@example.com>
2023-07-10 02:22:33 +0300

    Added calculator ex

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.lang.Math;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public double squareRoot(int num) {
+        return Math.sqrt(num);
+    }
+}
